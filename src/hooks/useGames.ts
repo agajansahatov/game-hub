1eca58e6a0b6a893b293b18bfbe4cf9dc94e6db7
Agajan Sahatov <agajan.st@gmail.com>
2024-11-26 21:03:28 +0800

    Implement infinite queries on the game grid

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,4 +1,4 @@
-import { useQuery } from "@tanstack/react-query";
+import { useInfiniteQuery } from "@tanstack/react-query";
 import { GameQuery } from "../App";
 import { Platform } from "./usePlatforms";
 import APIClient, { FetchResponse } from "../services/APIClient";
@@ -15,17 +15,21 @@ export interface Game {
 const apiClient = new APIClient<Game>('/games');
 
 const useGames = (gameQueryParams: GameQuery) => 
-	useQuery<FetchResponse<Game>, Error>({
+	useInfiniteQuery<FetchResponse<Game>, Error>({
 		queryKey: ['games', gameQueryParams],
-		queryFn: () => 
+		queryFn: ({pageParam = 1}) => 
 			apiClient.getAll({
 				params: {
 					genres: gameQueryParams.genre?.id, 
 					parent_platforms: gameQueryParams.platform?.id,
 					ordering: gameQueryParams.sortOrder,
-					search: gameQueryParams.searchText
+					search: gameQueryParams.searchText,
+					page: pageParam
 				}
-			})
+			}),
+		getNextPageParam: (lastPage, allPages) => {
+			return lastPage.next ? allPages.length + 1 : undefined;
+		}
 	});
 
 export default useGames;
